f51c2202c51ccff52cc8974ff91e11e50edd1b18
priyadarshimohanty01 <apkapotato@gmail.com>
2025-04-04 07:43:47 +0000

    container config done

---
+++ 2_datatypes.js
@@ -0,0 +1,24 @@
+"srtict mode" // it means it will use the newer versions.
+
+//alert("hello") // we are using node js not in browser 
+
+//primitive datatypes 
+
+/*
+1. numbers
+2. string
+3. boolean
+4. bigInt
+5. null
+6. symbols
+7. undefined
+*/
+
+//non-primitive datatypes
+// 1. object
+
+let age = 23
+let name = "Priyadarshi"
+let isFollow = true
+
+console.log(age, name, isFollow);
