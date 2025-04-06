3cce1e014e655c701699c8e9cb30d47033a23d76
priyadarshimohanty01 <apkapotato@gmail.com>
2025-04-06 02:07:43 +0000

    commit

---
+++ 3.Conversion.js
@@ -0,0 +1,21 @@
+let ab = "23"
+bc = Number(ab)
+console.log(bc);
+console.log(typeof(bc));
+
+
+let xy = "name123" 
+y = Number(xy)
+console.log(y);
+console.log(typeof(y));
+
+
+let isFollow = 1
+let boo = Boolean(isFollow)
+console.log(boo);
+console.log(typeof(boo));
+
+
+let s = 1 + false + "A"
+console.log(s);
+console.log(typeof(s));
---
+++ 4.Operators.js
@@ -0,0 +1,6 @@
+//There are 4 types of operators 
+
+//1.    Arithmetic operators
+//2.    Assignment operators
+//3.    Comparision operators
+//4.    Logical operators
