f00adf5eac93435224484592d357cde7b50d9cdd
priyadarshimohanty01 <apkapotato@gmail.com>
2025-04-08 04:54:47 +0000

    done

---
+++ 5.memory.js
@@ -0,0 +1,64 @@
+// primitive ---> stack
+// non primitive ---> heap
+
+//stack -----> original Value change habani
+
+let x = "      chui yugwsioyhws oisaypoiqsuipqh upqwsu;oqsj;lqjl        "
+let zzz = x  // zzz = chui
+zzz = "munu" // zzz ku change kalu
+console.log(x); // chui
+// value ku neiki kama 
+
+
+// heap ----> original value change karidada
+
+let obj = {
+    name : "priya",
+    age : 23
+}
+
+let yyy = obj // yyy = name : priya, age : 23
+yyy.age = 1234
+console.log(obj);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let
+var
+const
+
+const App=()=>{} // function
+
+
+
+console.log();
+console.table();
+parseInt();  // method
+
+<input type="text" ref={number1}/> <br/>
