e6eb86dd6b4939fb30cd5a83b88976b2471536b3
priyadarshimohanty01 <apkapotato@gmail.com>
2025-04-10 05:33:26 +0000

    done

---
+++ 6.string.js
@@ -0,0 +1,17 @@
+let name = "munu"
+let email = "bac@google.com"
+let Address = "Mahanadi Vihar, cuttack"
+
+
+// 1 Type
+//console.log("My name is "+name+ " and my email is "+email+". My address is "+Address); 
+
+// Another tYPES
+//console.log(`My name is ${name} and my email is ${email}. My address is ${Address} `); // another type
+
+
+// string and its methods 
+
+let a = "     Utkaila     "
+let b = a.trim
+console.log(b);
